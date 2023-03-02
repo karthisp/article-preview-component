2bc254d9a76d268abbccebce581fb3b2a5bc8c54
Karthik Sp <karthik.srinivasprasad@gmail.com>
2023-03-02 22:17:37 +0530

    adding functions to hide and show share buttons

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const shareBtn = document.querySelector('.share-btn');
 const shareSection = document.querySelector('.media-platforms');
 const shareArrow = document.querySelector('.pointing-arrow');
 
+function showElement(elementsList) {
+    elementsList.forEach(element => {
+        element.classList.add('hide-element')
+    });
+}
+
+function removeElement(elementsList) {
+    elementsList.forEach(element => {
+        element.classList.remove('hide-element')
+    });
+}
+
 shareBtn.addEventListener('click', function(){
     if(shareSection.classList.contains('hide-element')) {
-        shareSection.classList.remove('hide-element')
-        shareArrow.classList.remove('hide-element')
+        removeElement([shareSection, shareArrow])
     } else {
-        shareSection.classList.add('hide-element')
-        shareArrow.classList.add('hide-element')        
+        showElement([shareSection, shareArrow])      
     }
 })
